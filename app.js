060d60be2826b9938c0436ac8098d77edacc97bc
Caleb <87406555+caleblopez96@users.noreply.github.com>
2022-10-29 15:36:24 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -44,3 +44,21 @@ vampire.addEventListener("click", () => {
 btn.addEventListener('click', ()=>{
     window.location.reload()
 })
+
+/* creating the logic to check if winning options has been chosen reset everything and make everything unclickalbe
+
+
+checkStatus = () => {
+    if (winner.textContent.includes('🍬')){
+        return true
+    } 
+    return false
+}
+
+*/
+
+/* 
+
+creating the logic to rng which goul is the treat and which ghouls are the treat. right now the ghost is the treat every time. I want it to be random
+
+*/
